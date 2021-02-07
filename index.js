deb1dd8a3a4fcaac6f726ccef48ec9a91acaa05c
Ignacio <dev.ignacio.h@gmail.com>
2021-02-07 12:43:06 -0300

    definicion de rutas principales

--- index.js
+++ index.js
@@ -1,14 +1,34 @@
-/** Sintaxis de common.js */
-const express = require('express'); // se asigna a constante express importacion de express
+/* se asigna a constante express importacion de express */
 
-const app = express(); // se asigna a copnstante app función para ejecutar express
+// const express = require('express'); /* Sintaxis de common.js, es la sintaxis por default*/
+import express from 'express';         /* Sintaxis nativa se debe agregar "type": "module" a package.json */
+const app = express();                 /* se asigna a copnstante app la función para ejecutar express */
 
-// Definir puerto7
 /**
  * Se define el puerto y se le asigna proces.env.PORT (variable de entorno), en local no exite,
  * por lo tanto se asignará el puerto 4000
  */
 const port = process.env.PORT || 4000;
+
+/**
+ * método que recibe como argumentos una ruta y un callback que a su vez recibe como argumentos 
+ * un request y un response
+ */
+app.get('/', (req, res) => {     /* req = lo que enviamos - req = lo que express nos responde */  
+  res.send('Inicio');            /* Mustra algo en pantalla */
+  // res.json({                  /* Retorna una respuesta de json */
+  //   id: 1
+  // })
+  // res.render()                /* Retorna una vista */
+});
+
+app.get('/nosotros', (req, res) => {
+  res.send('Nosotros');
+})
+
+app.get('/contacto', (req, res) => {
+  res.send('Contacto');
+})
 
 /**
  * Se arranca el server con la función listen a la cual se le da como argumento el puerto
